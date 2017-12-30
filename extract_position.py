bee91330e6453df6bc941c0f8a1f6ba176616018
mrcheng0910 <mrcheng0910@gmail.com>
2017-12-30 10:29:37 +0800

    修改更新数据逻辑，加快更新速度

--- extract_position.py
+++ extract_position.py
@@ -6,8 +6,8 @@
 import sys
 import re
 # import MySQLdb as mdb
-from data_base import MySQL
-from config import SOURCE_CONFIG
+from get_whois.data_base import MySQL
+from get_whois.config import SOURCE_CONFIG
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -61,39 +61,43 @@ def extract_geo(domain, details):
         # reg_postal = mdb.escape_string(reg_postal) if reg_postal is not None else(reg_postal)
         # reg_country = mdb.escape_string(reg_country) if reg_country is not None else(reg_country)
 
-        print reg_country
-        print reg_postal
-        print reg_city
-        print reg_province
-        # return domain,details,reg_country,reg_province,reg_city,reg_postal
-        if reg_country:
-            update_db(reg_country,reg_postal,reg_city,reg_province,domain)
+        print reg_country,reg_province,reg_city,reg_postal
+        return reg_country,reg_province,reg_city,reg_postal
 
-def update_db(reg_country,reg_postal,reg_city,reg_province,domain):
-    db = MySQL(SOURCE_CONFIG)
+
+
+def update_db(db,reg_country, reg_postal, reg_city, reg_province,domain):
+    # db = MySQL(SOURCE_CONFIG)
     sql = 'UPDATE domain_info SET reg_country="%s",reg_postal = "%s",reg_city="%s",reg_province="%s" WHERE domain = "%s"'  % (reg_country,reg_postal,reg_city,reg_province,domain)
-    db.update(sql)
-    db.close()
+    db.update_no_commit(sql)
+    # db.close()
 
 
 def fetch_resource_data():
-    """获得源数据
-    :param tb_name: string 表名
-    :return: results: 查询结果
+    """
+    获取要解析的域名和whois详情
     """
     db = MySQL(SOURCE_CONFIG)
-    db.query('SELECT domain,details FROM domain_info')
+    db.query('SELECT domain,details FROM domain_info WHERE flag=1 AND reg_province IS NULL')
     results = db.fetch_all_rows()
     db.close()
     return results
 
+
 def manage_dw(dw):
-
+    """
+    处理数据
+    :param dw: 要处理的域名和其whois信息
+    """
+    db = MySQL(SOURCE_CONFIG)
     for domain, detail in dw:
-        print domain
-        # print domain,detail
-        extract_geo(domain, detail)
+        reg_country,reg_province,reg_city,reg_postal = extract_geo(domain, detail)
 
+        if reg_country:  # 包括非cn或china的国家
+            update_db(db, reg_country,reg_postal,reg_city,reg_province,domain)
+
+    db.commit()
+    db.close()
 
 
 if __name__ == '__main__':
